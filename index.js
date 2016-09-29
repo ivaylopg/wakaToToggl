c8ee1c67090f6266b39f5f7179307cea6efb931a
Ivaylo Getov <contact@ivaylogetov.com>
2016-09-29 16:42:27 -0700

    limit frequency of API requests

--- index.js
+++ index.js
@@ -24,19 +24,32 @@ function getTogglUserData() {
   })
 }
 
+
+// user data "default_wid"
+
 function processTogglData(body) {
   var togglData = JSON.parse(body)
   if (togglData === undefined || togglData.data === undefined || togglData.data.projects === undefined) {return};
-  var togglProjects = togglData.data.projects.map(function(data){
+
+  var togglProjects = togglData.data.projects.filter(function(data){
+    return data.server_deleted_at ? false : true
+  });
+
+  togglProjects = togglProjects.map(function(data){
     return {"id":data.id,"name": data.name,"default_wid":togglData.data.default_wid,"wid":data.wid};
   })
+
+  if (togglProjects.length === 0) {
+    togglProjects = [{"default_wid":togglData.data.default_wid}]
+  }
+
   //console.log(togglProjects);
   getWakatimeData(togglProjects);
 }
 
 function getWakatimeData(togglProjects) {
   var yesterday = new Date();
-  yesterday.setDate(yesterday.getDate() - 1)
+  yesterday.setDate(yesterday.getDate() - 5)
   yesterday = yesterday.toISOString().split(/T/)[0];
 
   var options = {
@@ -63,8 +76,16 @@ function processWakatimeData(body,togglProjects) {
   //console.log(wakaData)
   var wakaProjects = wakaData.data.map(function(data){
     return {"duration":data.duration,"project": data.project.replace(" ",""),"time":data.time};
+  }).filter(function(data){
+    // Projects less than 5 mins
+    if (data.duration < 300) {
+      return false;
+    }
+    return true;
   });
-  //console.log(togglProjects);
+
+  //console.log(wakaProjects);
+
   var defaultWID = togglProjects[0].default_wid;
   var wakaProjectNames = wakaProjects.map(function(data){
     return data.project;
@@ -82,88 +103,142 @@ function processWakatimeData(body,togglProjects) {
     //console.log(entriesForProject);
 
     for (var k = togglProjects.length - 1; k >= 0; k--) {
-      if (togglProjects[k].name === wakaProjectNames[i]) {
-        console.log("Add entries to existing project '%s'", togglProjects[k].name)
+      //console.log("%s <==> %s",togglProjects[k].name.toLowerCase(), wakaProjectNames[i].toLowerCase());
+      if (togglProjects[k].name.toLowerCase() === wakaProjectNames[i].toLowerCase()) {
+        //console.log("Add entries to existing project '%s'", togglProjects[k].name)
         addEntriesToProject(entriesForProject,togglProjects[k].id);
         break;
       }
       if (k === 0) {
-        console.log("Create project '%s' and add entries", wakaProjectNames[i])
-        createTogglProjectWithEntries(wakaProjectNames[i],entriesForProject,defaultWID);
+        //console.log("Add unfiled entries", wakaProjectNames[i])
+        addUnfiledEntries(wakaProjectNames[i],entriesForProject,defaultWID);
       }
     }
   }
 }
 
-function createTogglProjectWithEntries(name,entries,wid) {
+// function createTogglProjectWithEntries(name,entries,wid) {
+//   var options = {
+//       url: 'https://www.toggl.com/api/v8/projects',
+//       method: 'POST',
+//       headers: {
+//         "Content-Type": "application/json",
+//         "Authorization": "Basic " + new Buffer(process.env.TOGGLKEY + ":api_token").toString('base64'),
+//       },
+//       json: {
+//         "project":{
+//           "name":name,
+//           "wid":wid
+//         }
+//       }
+//   }
+
+//   // Start the request
+//   request(options, function (error, response, body) {
+//     if (!error) {
+//       //console.log("Create project response for %s:", name)
+//       //console.log(response.statusCode)
+//       //console.log(body)
+//       try {
+//         var responseData = JSON.parse(body);
+//         if (responseData.data === undefined) {
+//           console.error("Error creating project");
+//           return;
+//         }
+//         console.log("------------")
+//         console.log(responseData.data)
+//         var tProject = {
+//           "id": responseData.data.id,
+//           "name": responseData.data.name,
+//           "default_wid": wid,
+//           "wid": responseData.data.wid
+//         }
+//         console.log("new project")
+//         console.log(tProject)
+//         //console.log("created new project:")
+//         //console.log(tProject)
+//         //addTogglEntry(wProject,tProject)
+//         addEntriesToProject(entries,tProject.id);
+//       } catch(e) {
+//         console.log("error parsing json response from createTogglProjectWithEntry(). Response:")
+//         console.log(body)
+//       }
+//     } else {
+//       console.error(error)
+//     }
+//   });
+// }
+
+function addUnfiledEntries(name,entries,wid) {
+  for (var i=0, l=entries.length; i<l; i++) {
+    (function(n,e,w){
+      setTimeout(
+        function(){addUnfiledTogglEntry(n,e,w)}, i*1000);
+    })(name,entries[i],wid)
+  }
+}
+
+function addUnfiledTogglEntry(name,entry,wid) {
+  //console.log("ping - %s",name);
+
+  var startTime = new Date(entry.time * 1000).toISOString()
+  var timeEntry = {
+    "time_entry": {
+      "description":"Dev/Coding for " + name,
+      "tags":["wakaToToggl"],
+      "duration":entry.duration,
+      "start":startTime,
+      "wid":wid,
+      "created_with":"wakaToToggl"
+    }
+  }
+
   var options = {
-      url: 'https://www.toggl.com/api/v8/projects',
+      url: 'https://www.toggl.com/api/v8/time_entries',
       method: 'POST',
       headers: {
         "Content-Type": "application/json",
         "Authorization": "Basic " + new Buffer(process.env.TOGGLKEY + ":api_token").toString('base64'),
       },
-      json: {
-        "project":{
-          "name":name,
-          "wid":wid
-        }
-      }
+      json: timeEntry
   }
 
+
+  //console.log(options)
   // Start the request
   request(options, function (error, response, body) {
     if (!error) {
-      //console.log("Create project response for %s:", name)
       //console.log(response.statusCode)
-      //console.log(body)
-      try {
-        var responseData = JSON.parse(body);
-        if (responseData.data === undefined) {
-          console.error("Error creating project");
-          return;
-        }
-        console.log("------------")
-        console.log(responseData.data)
-        var tProject = {
-          "id": responseData.data.id,
-          "name": responseData.data.name,
-          "default_wid": wid,
-          "wid": responseData.data.wid
-        }
-        console.log("new project")
-        console.log(tProject)
-        //console.log("created new project:")
-        //console.log(tProject)
-        //addTogglEntry(wProject,tProject)
-        addEntriesToProject(entries,tProject.id);
-      } catch(e) {
-        console.log("error parsing json response from createTogglProjectWithEntry(). Response:")
-        console.log(body)
+      if (response.statusCode !== 200) {
+        console.log("----------------------------------")
+        console.log(response.statusCode)
+        console.log("Response For '%s': ",name);
+        console.log(response)
+        console.log("----------------------------------")
       }
+      //console.log(body)
     } else {
       console.error(error)
     }
   });
 }
 
-
 function addEntriesToProject(entries,projectID) {
   for (var i=0, l=entries.length; i<l; i++) {
-      addTogglEntry(entries[i],projectID);
+    (function(e,id){
+      setTimeout(
+        function(){addTogglEntry(e,id)}, i*1000);
+    })(entries[i],projectID)
   }
 }
 
 function addTogglEntry(wProject,projectID) {
-  if (wProject.duration < 60) {
-    //console.log("Project %s entry too short",wProject.project)
-    return;
-  }
+  //console.log("ping - %s",wProject.project);
 
   var startTime = new Date(wProject.time * 1000).toISOString()
   var timeEntry = {
     "time_entry": {
-      "description":"Dev/Coding",
+      "description":"Dev/Coding for " + wProject.project,
       "tags":["wakaToToggl"],
       "duration":wProject.duration,
       "start":startTime,
@@ -181,13 +256,23 @@ function addTogglEntry(wProject,projectID) {
       },
       json: timeEntry
   }
-  //console.log(options)
+
+  // console.log(options)
+  // return
+
   // Start the request
   request(options, function (error, response, body) {
     if (!error) {
       //console.log(response.statusCode)
-      console.log("Response For '%s' with ID %s",wProject.project,projectID);
-      console.log(body)
+      if (response.statusCode !== 200) {
+        console.log("----------------------------------")
+        console.log(response.statusCode)
+        console.log("Response For '%s' with ID %s",wProject.project,projectID);
+        console.log(response)
+        console.log("----------------------------------")
+      }
+
+      //console.log(body)
     } else {
       console.error(error)
     }
